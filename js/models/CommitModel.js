3054059a9f20ba4b5ce085a42755fe56e8e7a28a
Timo Taglieber <mail@timotaglieber.de>
2014-01-12 21:23:46 +0100

    Added CommitModel

---
+++ js/models/CommitModel.js
@@ -0,0 +1,20 @@
+/**
+ * Commit model.
+ *
+ * Initially contains commit data from the GitHub API, will later be enriched with app-specific data used for
+ * visually presenting file changes.
+ */
+(function () {
+    function Commit (data) {
+        this.author = data.commit.author;
+        this.committer = data.commit.committer;
+        this.message = data.commit.message;
+        this.sha = data.sha;
+    }
+    
+    Commit.prototype.printSha = function () {
+        console.log(this.sha);
+    }
+
+    GitBert.CommitModel = Commit;
+}());
