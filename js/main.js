3362eac568683dba4ea1c61992ba152829dc8d11
Timo Taglieber <mail@timotaglieber.de>
2014-01-12 21:24:14 +0100

    Instantiating CommitModels in main app controller

--- js/main.js
+++ js/main.js
@@ -3,6 +3,19 @@
  */
 (function () {
     GitBert.init = function (commits) {
+        // Store commits here by mapping the SHA to model instances.
+        GitBert.commits = {};
+        
+        // Preserve order by saving the SHAs in an array, where the first element will be the first commit.
+        GitBert.commitsOrder = [];
+        
+        // Loop over commits and create model instances.
+        _.each(commits, function (commitData) {
+            GitBert.commits[commitData.sha] = new GitBert.CommitModel(commitData);
+            GitBert.commitsOrder.push(commitData.sha);
+        });
+        GitBert.commitsOrder.reverse();
+        
         GitBert.statusView.showFetchingCommits(commits.length);
     };
 }());
