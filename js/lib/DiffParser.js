653d4e55c3e7f06ee6354ff480ce442950387110
Timo Taglieber <mail@timotaglieber.de>
2014-01-15 02:52:13 +0100

    Added DiffParser

---
+++ js/lib/DiffParser.js
@@ -0,0 +1,56 @@
+/**
+ * Diff parser singleton.
+ * 
+ * Returns an object representing the structure of a unified diff for a committed file.
+ */
+(function () {
+    GitBert.diffParser = {};
+    var parser = GitBert.diffParser;
+
+    var re = {
+        hunkHeader: /@@ -(\d+)(,\d+)? \+(\d+)(,\d+) @@/,
+    };
+    
+    parser.getHunkHeader = function (line) {
+        var matches = line.match(re.hunkHeader);
+        if (matches === null) {
+            return null;
+        }
+        return {
+            old: {
+                start: parseInt(matches[1]),
+                size: parseInt(matches[2].replace(',', ''))
+            },
+            new: {
+                start: parseInt(matches[3]),
+                size: parseInt(matches[4].replace(',', ''))
+            }
+        };
+    };
+
+    parser.parse = function (diff) {
+        var lines = diff.split('\n'),
+            hunkLines = [],
+            hunks = [],
+            prevHunk,
+            currHunk,
+            hunk;
+            
+        _.each(lines, function (line, index) {
+            // Check if we encountered the beginning of a hunk or the last line in the diff.
+            hunk = parser.getHunkHeader(line);
+            if (hunk || index === lines.length - 1) {
+                if (hunkLines.length >= 1) {
+                    // Save buffered lines, they belong to the previous hunk.
+                    currHunk.lines = hunkLines;
+                    hunkLines = []; // Reset line buffer.
+                    hunks.push(currHunk); // Save previous hunk with all its lines.
+                }
+                currHunk = hunk;
+            } else {
+                hunkLines.push(line); // Buffer current line.
+            }
+        });
+        return hunks;
+    };
+}());
