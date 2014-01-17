ebdbabc2ca9143750c8bbef64c8faa36506988b1
Timo Taglieber <mail@timotaglieber.de>
2014-01-17 02:58:41 +0100

    Added ContentController

---
+++ js/controllers/ContentController.js
@@ -0,0 +1,64 @@
+/**
+ * Content controller singleton.
+ * 
+ * Wires up UI input with the ContentView.
+ */
+(function () {
+    GitBert.contentController = {};
+    var controller = GitBert.contentController;
+    
+    var currentIndex,
+        numCommits;
+
+    /**
+     * Show the next younger commit.
+     */
+    var next = function () {
+        if (currentIndex < numCommits - 1) {
+            currentIndex++;
+        } else {
+            currentIndex = 0;
+        }
+        show();
+    };
+    
+    /**
+     * Show the next older commit.
+     */
+    var previous = function () {
+        if (currentIndex >= 1) {
+            currentIndex--;
+        } else {
+            currentIndex = numCommits - 1;
+        }
+        show();
+    };
+    
+    var show = function () {
+        var sha = GitBert.commitsOrder[currentIndex];
+        GitBert.contentView.renderCommitBySha(sha);
+    };
+    
+    var checkKey = function (keyDownEvent) {
+        keyDownEvent = keyDownEvent || window.event;
+    
+        // Left arrow
+        if (keyDownEvent.keyCode === 37) {
+            previous();
+        }
+    
+        // Right arrow
+        if (keyDownEvent.keyCode === 39) {
+            next();
+        }
+    };
+    
+    /**
+     * Enable switching between commits with left and right arrow keys.
+     */
+    controller.init = function () {
+        $(document).keydown(checkKey);
+        currentIndex = 0;
+        numCommits = GitBert.commitsOrder.length;
+    };
+}());
